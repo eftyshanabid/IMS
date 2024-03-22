a1b2416d1153baaad4dae532007b6513c94fa352
MAHMUDHASANSADI <mhasan181075@bscse.uiu.ac.bd>
2024-03-22 23:36:24 +0600

    department CRUD

--- Modules/IMS/app/Http/Controllers/DepartmentController.php
+++ Modules/IMS/app/Http/Controllers/DepartmentController.php
@@ -4,25 +4,82 @@ namespace Modules\IMS\app\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\RedirectResponse;
+use Modules\IMS\app\Models\Departments;
+use Illuminate\Support\Facades\DB;
+use Yajra\DataTables\Facades\DataTables;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
 class DepartmentController extends Controller
 {
+    public function headerColumns($deleted = false)
+    {
+        return array(
+            ['SL', 'SL'],
+            ['name', 'name'],
+            ['code', 'code'],
+            ['status', 'status'],
+            ['actions', 'actions', 'text-center', 'width: 20% !important']
+        );
+    }
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        return view('ims::index');
+        $departments = Departments::when(!datatableOrdering(), function ($query) {
+            return $query->orderby('id', 'desc');
+        });
+
+        $options = [
+            'department-edit' => auth()->user()->hasPermissionTo('department-edit'),
+            'department-delete' => auth()->user()->hasPermissionTo('department-delete'),
+        ];
+        try {
+            if (request()->ajax()) {
+                return Datatables::of($departments)
+                    ->addIndexColumn()
+                    ->editColumn('status', function ($department) {
+                        return ucfirst($department->status);
+                    })
+                    ->addColumn('actions', function ($department) use ($options) {
+                        $actions = '';
+
+                        $actions .= '<a href="javascript:void(0)" onclick="return showDetails(' . $department->id . ')" class="btn btn-info btn-sm mb-2"><i class="mdi mdi-eye" title="Click to view details"></i></a> ';
+
+                        if ($options['department-edit']) {
+                            $actions .= '<a href="' . route('departments.edit', $department->id) . '" class="btn btn-warning btn-sm mb-2"><i
+                        class="mdi mdi-pencil-box" title="Click to Edit"></i></a> ';
+                        }
+                        if ($options['department-delete']) {
+                            $actions .= '<a class="btn btn-sm btn-danger mb-2" onclick="deleteFromCRUD($(this))" data-src="' . route('departments.destroy', $department->id) . '"><i class="mdi mdi-trash-can"></i></a>';
+                        }
+
+                        return $actions;
+                    })
+                    ->rawColumns(['actions'])
+                    ->make(true);
+            }
+
+            return view('ims::departments.index', [
+                'title' => 'Departments',
+                'headerColumns' => $this->headerColumns()
+            ]);
+        } catch (\Throwable $th) {
+            return $this->backWithError($th->getMessage());
+        }
     }
+
 
     /**
      * Show the form for creating a new resource.
      */
     public function create()
     {
-        return view('ims::create');
+        $title = "Create Departments";
+        $code = uniqueCode(6, 'D-', 'departments', 'id');
+
+        return view('ims::departments.create', compact('title', 'code'));
     }
 
     /**
@@ -30,7 +87,23 @@ class DepartmentController extends Controller
      */
     public function store(Request $request): RedirectResponse
     {
-        //
+        $this->validate($request, [
+            'name' => 'required',
+            'code' => 'required',
+        ]);
+
+        DB::beginTransaction();
+        try {
+            $department = new Departments();
+            $department->fill($request->all());
+            $department->save();
+
+            DB::commit();
+            return $this->redirectBackWithSuccess('Department created successfully', 'departments.index');
+        } catch (\Exception $e) {
+            DB::rollback();
+            return $this->backWithError($e->getMessage());
+        }
     }
 
     /**
@@ -38,7 +111,11 @@ class DepartmentController extends Controller
      */
     public function show($id)
     {
-        return view('ims::show');
+        $title = "Departments Show";
+
+        $department = Departments::findOrFail($id);
+
+        return view('ims::departments.show', compact('department', 'title'));
     }
 
     /**
@@ -46,7 +123,11 @@ class DepartmentController extends Controller
      */
     public function edit($id)
     {
-        return view('ims::edit');
+        $title = "Department Edit";
+
+        $department = Departments::findOrFail($id);
+
+        return view('ims::departments.edit', compact('department', 'title'));
     }
 
     /**
@@ -54,7 +135,25 @@ class DepartmentController extends Controller
      */
     public function update(Request $request, $id): RedirectResponse
     {
-        //
+
+        $this->validate($request, [
+            'name' => 'required',
+            'code' => "required",
+        ]);
+
+
+        DB::beginTransaction();
+        try {
+            $department = Departments::findOrFail($id);
+            $department->fill($request->all());
+            $department->save();
+
+            DB::commit();
+            return $this->redirectBackWithSuccess('Departments updated successfully', 'departments.index');
+        } catch (\Exception $e) {
+            DB::rollback();
+            return $this->backWithError($e->getMessage());
+        }
     }
 
     /**
@@ -62,6 +161,26 @@ class DepartmentController extends Controller
      */
     public function destroy($id)
     {
-        //
+
+        DB::beginTransaction();
+        try {
+            $department = Departments::findOrFail($id);
+            $department->delete();
+
+            DB::commit();
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Departments has been deleted successfully',
+            ]);
+
+        } catch (\Exception $e) {
+            DB::rollback();
+            return response()->json([
+                'success' => false,
+                'message' => $e->getMessage()
+            ]);
+        }
     }
+
 }
---
+++ Modules/IMS/resources/views/departments/show.blade.php
@@ -0,0 +1,21 @@
+<div class="form-group row">
+    <div class="col-md-12">
+        <table class="table table-bordered table-striped table-hover">
+            <tbody>
+                <tr>
+                    <td>{{translate('Name')}}:</td>
+                    <td>{!! languageValues($department->name) !!}</td>
+                </tr>
+            <tr>
+                <td>{{translate('Code')}}:</td>
+                <td> {!! $department->code !!} $</td>
+            </tr>
+            <tr>
+                <td>{{translate('Status')}}:</td>
+                <td> {{ ucfirst($department->status) }}</td>
+            </tr>
+            </tbody>
+
+        </table>
+    </div>
+</div>
